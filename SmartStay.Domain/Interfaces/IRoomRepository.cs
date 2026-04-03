c5e925723f91533955863558a1e3b584f5467af8
Marko Grubisic <markogrubisic2@gmail.com>
2026-04-03 13:54:05 +0200

    added interfaces for repository and implementation for userRepo

---
+++ SmartStay.Domain/Interfaces/IRoomRepository.cs
@@ -0,0 +1,14 @@
+﻿using SmartStay.Domain.Entities;
+using SmartStay.Domain.Enums;
+
+namespace SmartStay.Domain.Interfaces;
+
+public interface IRoomRepository : IRepository<Room>
+{
+    Task<Room> GetRoomByNameAsync(string name);
+    Task<Room> GetRoomByCapacityAsync(int capacity);
+    Task<Room> GetRoomByPrice(float price);
+    Task<Room> GetRoomBySize(int size);
+    Task<Room> GetRoomByBedType(BedType bedType);
+    Task<Room> GetRoomByAverageRating(float averageRating);
+}
---
+++ SmartStay.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartStay.Domain.Entities;
+using SmartStay.Domain.Interfaces;
+using SmartStay.Infrastructure.Persistance;
+
+namespace SmartStay.Infrastructure;
+
+public class UserRepository: Repository<User>, IUserRepository
+{
+    public UserRepository(SmartStayDbContext dbContext) : base(dbContext)
+    {}
+    
+    public async Task<User?> GetUserByEmailAsync(string email)
+    => await _dbSet.FirstOrDefaultAsync(u => u.Email==email);
+    
+}
